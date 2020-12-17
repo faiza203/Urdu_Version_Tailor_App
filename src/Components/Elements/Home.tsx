b650d7c95df153d5ddc98650849a2eaa51d17783
faiza203 <fiazfaiza981@gmail.com>
2020-12-17 19:23:04 +0500

    Create SignUp.tsx

--- src/Components/Elements/Home.tsx
+++ src/Components/Elements/Home.tsx
@@ -1,7 +1,7 @@
 export function Home() {
     return (
         <div className='homePage' >
-            <h1 className="h1 font-italic text-muted">Welcome to Tailor App</h1>
+            <h1 className="h1 font-italic text-muted">درزی ایپ میں خوش آمدید</h1>
             <div>
             </div>
             <img height="350" src="https://images-na.ssl-images-amazon.com/images/I/41aTFrD3D9L._SX425_.jpg" alt="" />
