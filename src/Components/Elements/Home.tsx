a2ddb1de1a242628bff20a051e2ae2e2e0ab803d
faiza203 <fiazfaiza981@gmail.com>
2020-12-17 18:57:56 +0500

    Create Home Element

---
+++ src/Components/Elements/Home.tsx
@@ -0,0 +1,10 @@
+export function Home() {
+    return (
+        <div className='homePage' >
+            <h1 className="h1 font-italic text-muted">Welcome to Tailor App</h1>
+            <div>
+            </div>
+            <img height="350" src="https://images-na.ssl-images-amazon.com/images/I/41aTFrD3D9L._SX425_.jpg" alt="" />
+        </div>
+    )
+}
