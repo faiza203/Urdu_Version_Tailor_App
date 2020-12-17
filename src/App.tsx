0a1b9742b9e5815b0b347f91609f7d5a9c3cdb73
faiza203 <fiazfaiza981@gmail.com>
2020-12-17 19:20:33 +0500

    Add Homer in Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Router, Route, Switch } from 'react-router';
 import './App.css';
 import { Home } from './Components/Elements';
+import { history } from './Components/history';
+
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router history={history}>
+        <Switch>
+          <Route path="/" component={Home} exact></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
