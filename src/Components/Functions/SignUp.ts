a5bf3e99ad781be8c7dac1263f9d5043557abc3d
faiza203 <fiazfaiza981@gmail.com>
2020-12-18 15:29:49 +0500

    Add function to sign up with firebase

--- src/Components/Functions/SignUp.ts
+++ src/Components/Functions/SignUp.ts
@@ -1,8 +1,18 @@
+import { findAllByDisplayValue } from '@testing-library/react';
 import firebase from 'firebase';
 import { config } from './../firebase';
 config();
 
+const auth = firebase.auth();
 export const SignUpFun = (e: any) => {
     e.preventDefault();
-    console.log(firebase.auth());
+    const [emailInput, passwordInput, confirmPasswordInput] = e.target;
+    const [email, password, confirmPassword] = [emailInput.value, passwordInput.value, confirmPasswordInput.value];
+    if (password !== confirmPassword) {
+        alert("Your password does not matched")
+    } else if (email === password) {
+        alert("Your have write email as password")
+    } else {
+        auth.createUserWithEmailAndPassword(email, password).then((user) => { console.log(user) }).catch((err) => alert(err.message))
+    }
 }
