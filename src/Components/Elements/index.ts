78d89270cb4926d33baac59439f1f96e3b8a639c
faiza203 <fiazfaiza981@gmail.com>
2020-12-19 17:49:00 +0500

    Create Measurment Element

--- src/Components/Elements/index.ts
+++ src/Components/Elements/index.ts
@@ -3,3 +3,4 @@ export { SignUp } from './SignUp';
 export { SignIn } from './SignIn';
 export { DashBoard } from './DashBoard';
 export { Customers } from './Customer';
+export { Measurment } from './Measurment'
