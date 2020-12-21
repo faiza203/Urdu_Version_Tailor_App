85555e0571ca2d0697b36676aac2d698f94346f8
faiza203 <fiazfaiza981@gmail.com>
2020-12-21 14:16:39 +0500

    Create Condition.ts Function

--- src/Components/Elements/EditMeasurment.tsx
+++ src/Components/Elements/EditMeasurment.tsx
@@ -39,8 +39,7 @@ export function EditMeasurment() {
                     alert(err.message);
                     dispatch(deleteClientR());
                 })
-            }
-
+            } 
         });
     }
     return (
